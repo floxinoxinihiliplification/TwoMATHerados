d5606e50a23e32fb1c50d218e1e233db3568d544
ivanmiha.heroj@gmail.com <ivanmiha.heroj@gmail.com>
2022-05-02 23:21:43 +0200

    Setting the inital scene

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField, Range(1f, 5f)] private float speed = 5f;
+
+    private float vertical;
+
+    private void Update()
+    {
+        GetInput();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GetInput()
+    {
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void Move()
+    {
+        Vector3 changeInPosition = new Vector3(0f, 0f, vertical);
+        Vector3 goToPositon = transform.position + changeInPosition * speed * Time.deltaTime;
+        GetComponent<Rigidbody>().MovePosition(goToPositon);
+    }
+}
