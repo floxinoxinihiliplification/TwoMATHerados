a2902e4a2e4e3ae7c4bd85f0f517463ab2057875
sandramii <sandramilenkovicc@gmail.com>
2022-05-25 21:02:08 +0200

    Enemy Destroy

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private int damage;
+    public int damage;
 
     public void Setup(int damage)
     {
-        this.damage = damage; 
+        this.damage = damage;
         GetComponent<Rigidbody>().velocity = transform.forward * speed;
     }
     
-    
+    private void OnTriggerEnter(Collider other)
+{
+    if (other.CompareTag("Zombiee"))  
+    {
+        // Destroy the thing tagged enemy, not youself
+        Destroy(other.gameObject);
+
+        // Could still destroy the bullet itself as well
+        Destroy (gameObject);
+    }
+}
 
     private void OnCollisionEnter(Collision other) 
     {
         if(other != null)
         {
-            Debug.Log("col: " + other.collider.name);
             Destroy(gameObject);
         }   
     }
